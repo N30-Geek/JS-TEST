3e80099375228f008995815013b792fc89632d49
Néo Coolman <76451391+N30-Geek@users.noreply.github.com>
2024-07-25 02:38:12 +0200

    pratique de slide avec swiper library

---
+++ buttons_css/main.js
@@ -0,0 +1,11 @@
+import {signal} from "@preact/signals-core"
+
+const  firstname = singal("John");
+// si on veut lire la valeur
+firstname.value 
+
+
+// si on veut changer la veleur
+firstname.value = "Jane"
+
+
